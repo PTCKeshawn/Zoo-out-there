698acde0f1a63a67ca0818440c8fe06686f11473
PTCKeshawn <IChidiOnor@smail.pembinatrails.ca>
2025-05-23 11:21:52 -0500

    added a resetting password function so now we can reset passwords

--- user_configuration.sh
+++ user_configuration.sh
@@ -25,28 +25,48 @@ new_user() {
 	userPassword: $pass" > users.ldif 
 
 	ldapadd -x -D "cn=admin,dc=zoo,dc=local" -W -f users.ldif
+	echo "creating user..."
+	sleep 2
+	echo "done!"
+	sleep 1
 }
 del_user() {
 	read -p "what is the users uid :" uids
 	ldapdelete -x -D "cn=admin,dc=zoo,dc=local" -W "uid=$uids,dc=zoo,dc=local"
+	echo "deleting..."
+	sleep 2
 	echo "done!"
+	sleep 1
+}
+reset_password() {
+	read -p "what is the users uid :" uidss
+	sudo ldappasswd -x -D "cn=admin,dc=zoo,dc=local" -W \
+  -S "uid=$uidss,dc=zoo,dc=local"
+  	echo "resetting $uidss's password..."
+  	sleep 2
+  	echo "done"
+  	sleep 1
+
 }
 
 while [ "$script" = 1 ]; do
-	read -p "do you want to create a user or delete a user:(1=create.2=delete.3=exit)" what
+	read -p -e "do you want to : \ 1.create a user \ 2.delete a user \3 .reset a password \4.exit" what
 	
 	if [[ "$what" = "1" ]]; then
 		new_user
 	elif [[ "$what" = "2" ]]; then
 		del_user
 	elif [[ "$what" = "3" ]]; then
+		reset_password
+	elif [[ "$what" = "4" ]]; then
 		echo "Exiting..."
 		script=0
 
 	else 
-		echo "invalid. pick 1, 2, or 3"
+		echo "invalid. pick 1, 2, 3, or 4"
 		exit 0
 	fi
 done
+#remember to continue from Fsalazar today on may 23
 
 
